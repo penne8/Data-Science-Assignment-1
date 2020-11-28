de35b1d7f1a1b42d6208d0e449c0276f00149849
Pan Eyal <paney@post.bgu.ac.il>
2020-11-28 15:36:49 +0200

    insert to main

--- Question4.py
+++ Question4.py
@@ -1,12 +1,18 @@
 import numpy as np
 
-A = np.asarray([[5,6,7,8],[1,3,5,4],[1,0.5,4,2],[3,4,3,1]])
-B = np.asarray([[0.57,0.56,0.8,1],[1.5,4,6.7,4.9],[0.2,0.1,1,0.6],[11,30,26,10]])
+def main():
 
-# find least square solution
-D = np.zeros((4, 4))
-for i in range(0, 4):
-    ACoefficients = A[i] @ np.transpose([A[i]])
-    BCoefficients = A[i] @ np.transpose([B[i]])
-    D[i, i] = BCoefficients/ACoefficients
-print(D)
+    A = np.asarray([[5, 6, 7, 8], [1, 3, 5, 4], [1, 0.5, 4, 2], [3, 4, 3, 1]])
+    B = np.asarray([[0.57, 0.56, 0.8, 1], [1.5, 4, 6.7, 4.9], [0.2, 0.1, 1, 0.6], [11, 30, 26, 10]])
+
+    matrix_size = len(B)
+    # find least square solution
+    D = np.zeros((matrix_size, matrix_size))
+    for i in range(matrix_size):
+        ACoefficients = A[i] @ np.transpose([A[i]])
+        BCoefficients = A[i] @ np.transpose([B[i]])
+        D[i, i] = BCoefficients / ACoefficients
+    print(D)
+
+if __name__ == '__main__':
+    main()
