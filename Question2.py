87a9d5a6360f3fc4d478c175c32750744141cbb5
penne8 <paney@post.bgu.ac.il>
2020-11-28 18:41:25 +0200

    Error fixes

--- Question2.py
+++ Question2.py
@@ -4,17 +4,21 @@ import numpy as np
 minimal_proximity = 1.00000000e-03
 satisfy_equation_step = 0.01
 
+
 def least_square(A, B):
-    A_transpose = np.transpose(A)
-    x = np.linalg.solve(A_transpose @ A, A_transpose @ B)
+    a_transpose = np.transpose(A)
+    x = np.linalg.solve(a_transpose @ A, a_transpose @ B)
     return x
+
 
 def minimal_loss_vector(A, x, B):
     curr_minimal_loss_vector = (A @ x) - B
     return curr_minimal_loss_vector
 
+
 def minimal_loss(r):
     return np.transpose(r) @ r
+
 
 def sanity_check(A, r):
     numerical_zero = np.transpose(A) @ r
@@ -24,33 +28,35 @@ def sanity_check(A, r):
             negligible = False
     return negligible
 
+
 def satisfy_equation(A, B, r, equation):
-    W = np.eye(len(A))
+    w = np.eye(len(A))
+    x = np.empty(len(A))
     while np.absolute(r[equation]) >= minimal_proximity:
-        W[equation, equation] += satisfy_equation_step
-        A_transpose = np.transpose(A)
-        x = np.linalg.solve(A_transpose @ W @ A, A_transpose @ W @ B)
+        w[equation, equation] += satisfy_equation_step
+        a_transpose = np.transpose(A)
+        x = np.linalg.solve(a_transpose @ w @ A, a_transpose @ w @ B)
         r = minimal_loss_vector(A, x, B)
-    print(f"the 1st time that r_{equation + 1} is smaller then {minimal_proximity} is when W= ", W[equation, equation])
-    print("sanity check passed: ", sanity_check(A, W @ r))
+    print(f"the 1st time that r_{equation + 1} is smaller then {minimal_proximity} is when w= ", w[equation, equation])
+    print("sanity check passed: ", sanity_check(A, w @ r))
     return x
 
-def main():
 
-    A = np.asarray([[2, 1, 2], [1, -2, 1], [1, 2, 3], [1, 1, 1]])
-    B = np.asarray([6, 1, 5, 2])
+def main():
+    a = np.asarray([[2, 1, 2], [1, -2, 1], [1, 2, 3], [1, 1, 1]])
+    b = np.asarray([6, 1, 5, 2])
 
     # Finding LS solution
-    least_square_solution = least_square(A, B)
+    least_square_solution = least_square(a, b)
     print("\nthis is the LS solution: \n", least_square_solution)
 
     # find minimal loss and validate
-    curr_minimal_loss_vector = minimal_loss_vector(A, least_square_solution, B)
+    curr_minimal_loss_vector = minimal_loss_vector(a, least_square_solution, b)
     print("the minimal loss (error) is: ", minimal_loss(curr_minimal_loss_vector))
-    print("sanity check passed: ", sanity_check(A, curr_minimal_loss_vector))
+    print("sanity check passed: ", sanity_check(a, curr_minimal_loss_vector))
 
     # find minimal r such that r < minimal_proximity
-    least_square_solution = satisfy_equation(A, B, curr_minimal_loss_vector, 0)
+    least_square_solution = satisfy_equation(a, b, curr_minimal_loss_vector, 0)
     print("the least square solution is: ", least_square_solution)
 
 
--- Question5.py
+++ Question5.py
@@ -10,6 +10,7 @@ population = 3
 density = 4
 minimal_proximity = 1.00000000e-03
 
+
 def make_matrix(data_name, data_per_country_size, countries_size):
     countries_data = np.zeros((data_per_country_size, countries_size))
     with open(data_name) as csv_file:
@@ -24,21 +25,26 @@ def make_matrix(data_name, data_per_country_size, countries_size):
                 country_id += 1
     return countries_data
 
+
 def split_first(matrix, from_x, from_y, to_x, to_y):
     return matrix[from_x:to_x, from_y:to_y]
 
+
 def split_second(matrix, from_x, from_y, to_x, to_y):
     return matrix[from_x:to_x, from_y:to_y]
+
 
 def least_square(A, B):
     A_transpose = np.transpose(A)
     x = np.linalg.solve(A_transpose @ A, A_transpose @ B)
     return x
 
+
 def minimize_matrix(matrix, ignore_array, row_or_column_index):
     for elem in ignore_array:
         matrix = np.delete(matrix, elem, row_or_column_index)
     return matrix
+
 
 def minimize_by_average_rows(matrix, average_by):
     column_size = len(matrix[0])
@@ -67,20 +73,25 @@ def minimize_by_average_rows(matrix, average_by):
 
     return average_matrix
 
-def minimal_loss_vector(A, x, B):
-    curr_minimal_loss_vector = (A @ x) - B
+
+def minimal_loss_vector(a, x, b):
+    curr_minimal_loss_vector = (a @ x) - b
     return curr_minimal_loss_vector
 
-def minimal_loss(r):
+
+def vector_norm(r):
     return np.transpose(r) @ r
+
 
 def sanity_check(A, r):
     numerical_zero = np.transpose(A) @ r
     negligible = True
+
     for value in numerical_zero:
         if value > minimal_proximity:
             negligible = False
     return negligible
+
 
 def main():
     # Organize Country Data
@@ -89,18 +100,16 @@ def main():
     validation_countries_data = split_second(countries_data, 189, 0, 239, 7)
     # Organize France Data
     france_data = make_matrix('France.csv', 238, 1)
-    base_France_data = split_first(france_data, 0, 0, 189, 1)
-    validation_France_data = split_second(france_data, 189, 0, 239, 1)
-
-
+    base_france_data = split_first(france_data, 0, 0, 189, 1)
+    validation_france_data = split_second(france_data, 189, 0, 239, 1)
 
     # Finding LS solution
-    least_square_solution = least_square(base_countries_data, base_France_data)
+    least_square_solution = least_square(base_countries_data, base_france_data)
     print("\nthis is the simple minimal LS solution: \n", least_square_solution)
 
     # find minimal loss and validate
-    curr_minimal_loss_vector = minimal_loss_vector(base_countries_data, least_square_solution, base_France_data)
-    print("the minimal loss (error) is: ", minimal_loss(curr_minimal_loss_vector))
+    curr_minimal_loss_vector = minimal_loss_vector(base_countries_data, least_square_solution, base_france_data)
+    print("the minimal loss (error) is: ", vector_norm(curr_minimal_loss_vector))
     print("sanity check passed: ", sanity_check(base_countries_data, curr_minimal_loss_vector))
 
     # we noticed some countries are not correlated to France, therefore we will remove them:
@@ -115,8 +124,8 @@ def main():
     validation_countries_data = minimize_matrix(validation_countries_data, ignore_countries, 1)
 
     # evaluate Future data
-    validation_France_excepted = validation_countries_data @ least_square_solution
-    plt.plot(validation_France_excepted)
+    validation_france_excepted = validation_countries_data @ least_square_solution
+    plt.plot(validation_france_excepted)
     plt.xlabel('days')
     plt.ylabel('New Cases')
     plt.show()
@@ -124,29 +133,39 @@ def main():
     # we noticed there are a lot of noise in the graph, we will minimize it by taking the average of every week
     base_countries_data = minimize_by_average_rows(base_countries_data, 7)
     validation_countries_data = minimize_by_average_rows(validation_countries_data, 7)
-    base_France_data = minimize_by_average_rows(base_France_data, 7)
-    validation_France_data = minimize_by_average_rows(validation_France_data, 7)
+    base_france_data = minimize_by_average_rows(base_france_data, 7)
+    validation_france_data = minimize_by_average_rows(validation_france_data, 7)
 
     # Finding LS solution again
-    least_square_solution = least_square(base_countries_data, base_France_data)
+    least_square_solution = least_square(base_countries_data, base_france_data)
     print("\nthis is the minimal LS solution after average: \n", least_square_solution)
 
     # find minimal loss and validate
-    curr_minimal_loss_vector = minimal_loss_vector(base_countries_data, least_square_solution, base_France_data)
-    print("the minimal loss (error) is: ", minimal_loss(curr_minimal_loss_vector))
+    curr_minimal_loss_vector = minimal_loss_vector(base_countries_data, least_square_solution, base_france_data)
+    print("the minimal loss (error) is: ", vector_norm(curr_minimal_loss_vector))
     print("sanity check passed: ", sanity_check(base_countries_data, curr_minimal_loss_vector))
 
     # evaluate Future data
-    validation_France_excepted = validation_countries_data @ least_square_solution
-    plt.plot(validation_France_excepted)
+    validation_france_excepted = validation_countries_data @ least_square_solution
+    plt.plot(validation_france_excepted)
     plt.xlabel('days')
     plt.ylabel('New Cases')
     plt.show()
 
-    plt.plot(validation_France_data)
+    # find minimal loss of expected future data
+    curr_minimal_loss_vector = minimal_loss_vector(validation_countries_data, least_square_solution
+                                                   , validation_france_data)
+    print("the minimal loss (error) of expected future data is: ", vector_norm(curr_minimal_loss_vector))
+    print("sanity check passed: ", sanity_check(validation_countries_data, curr_minimal_loss_vector))
+
+    plt.plot(validation_france_data)
     plt.xlabel('days')
     plt.ylabel('New Cases')
     plt.show()
+
+    print("the ratio between our model error to the real france data is: ",
+          vector_norm(curr_minimal_loss_vector)/vector_norm(validation_france_data))
+
 
 if __name__ == '__main__':
     main()
