be20c9c6117f2b591f6e480a5e313af2b3e5136f
Pan Eyal <paney@post.bgu.ac.il>
2020-11-28 15:33:12 +0200

    making functions instead of repetitive code

--- Question2.py
+++ Question2.py
@@ -1,33 +1,59 @@
 import numpy as np
 
-A = np.asarray([[2, 1, 2], [1, -2, 1], [1, 2, 3], [1, 1, 1]])
-b = np.asarray([6, 1, 5, 2])
+# consts
+minimal_proximity = 1.00000000e-03
+satisfy_equation_step = 0.01
 
-# find least square solution
-Atranspose = np.transpose(A)
-ACoefficients = Atranspose @ A
-BResults = Atranspose @ b
-x = np.linalg.solve(ACoefficients, BResults)
-print("the least square solution is: ",  x)
+def least_square(A, B):
+    A_transpose = np.transpose(A)
+    x = np.linalg.solve(A_transpose @ A, A_transpose @ B)
+    return x
 
-# find minimal loss
-r = (A @ x) - b
-minError = r @ np.transpose(r)
-print("the minimal loss (error) is: ", minError)
-zeroMat = Atranspose @ r
-print("sanity check, should be close enough to 0: ", zeroMat)
+def minimal_loss_vector(A, x, B):
+    curr_minimal_loss_vector =  (A @ x) - B
+    return curr_minimal_loss_vector
 
-# find minimal r such that r<10^-3
-W = np.eye(4)
-while np.absolute(r[0]) >= 1.00000000e-03:
-    W[0, 0] = W[0, 0]+0.01
-    ACoefficients = Atranspose @ W @ A
-    BResults = Atranspose @ W @ b
-    x = np.linalg.solve(ACoefficients, BResults)
-    r = (A @ x) - b
-print("the 1st time that r[1] is smaller then 10^-3 is when W= ", W[0, 0])
-print("the least square solution is: ",  x)
-minError = r @ np.transpose(r)
-print("the minimal loss (error) is: ", minError)
-zeroMat = Atranspose @ W @ r
-print("sanity check, should be close enough to 0: ", zeroMat)
+
+def minimal_loss(r):
+    return np.transpose(r) @ r
+
+def sanity_check(A, r):
+    numerical_zero = np.transpose(A) @ r
+    negligible = True
+    for value in numerical_zero:
+        if value > minimal_proximity:
+            negligible = False
+    return negligible
+
+def satisfy_equation(A, B, r, equation):
+    W = np.eye(len(A))
+    while np.absolute(r[equation]) >= minimal_proximity:
+        W[equation, equation] += satisfy_equation_step
+        A_transpose = np.transpose(A)
+        x = np.linalg.solve(A_transpose @ W @ A, A_transpose @ W @ B)
+        r = minimal_loss_vector(A, x, B)
+    print(f"the 1st time that r_{equation + 1} is smaller then {minimal_proximity} is when W= ", W[equation, equation])
+    print("sanity check passed: ", sanity_check(A, W @ r))
+    return x
+
+def main():
+
+    A = np.asarray([[2, 1, 2], [1, -2, 1], [1, 2, 3], [1, 1, 1]])
+    B = np.asarray([6, 1, 5, 2])
+
+    # Finding LS solution
+    least_square_solution = least_square(A, B)
+    print("\nthis is the LS solution: \n", least_square_solution)
+
+    # find minimal loss and validate
+    curr_minimal_loss_vector = minimal_loss_vector(A, least_square_solution, B)
+    print("the minimal loss (error) is: ", minimal_loss(curr_minimal_loss_vector))
+    print("sanity check passed: ", sanity_check(A, curr_minimal_loss_vector))
+
+    # find minimal r such that r < minimal_proximity
+    least_square_solution = satisfy_equation(A, B, curr_minimal_loss_vector, 0)
+    print("the least square solution is: ", least_square_solution)
+
+
+if __name__ == '__main__':
+    main()
